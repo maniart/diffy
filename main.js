0fb55710fe04afa2a50478ae63b0822fcab6df2a
maniart <mani.art@gmail.com>
2016-03-23 01:00:08 -0400

    basic video capture and draw

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 'use strict';
 
+/*
+  shim requestAnimationFrame api
+  source: http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
+*/
 window.requestAnimFrame = (function(){
   return
     window.requestAnimationFrame       ||
@@ -13,15 +17,81 @@ window.requestAnimFrame = (function(){
 })();
 
 /*
+  shim getUserMedia with a Promise api
+  source: https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+*/
+function getUserMedia(constraints, successCallback, errorCallback) {
+
+  // First get ahold of getUserMedia, if present
+  var getUserMedia = (navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia);
+
+  // Some browsers just don't implement it - return a rejected promise with an error
+  // to keep a consistent interface
+  if(!getUserMedia) {
+    return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+  }
+
+  // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+  return new Promise(function(successCallback, errorCallback) {
+    getUserMedia.call(navigator, constraints, successCallback, errorCallback);
+  });
+
+}
+
+// Older browsers might not implement mediaDevices at all, so we set an empty object first
+if(navigator.mediaDevices === undefined) {
+  navigator.mediaDevices = {};
+}
+
+// Some browsers partially implement mediaDevices. We can't just assign an object
+// with getUserMedia as it would overwrite existing properties.
+// Here, we will just add the getUserMedia property if it's missing.
+if(navigator.mediaDevices.getUserMedia === undefined) {
+  navigator.mediaDevices.getUserMedia = getUserMedia;
+}
+
+
+// Prefer camera resolution nearest to 1280x720.
+var constraints = {
+  audio: false,
+  video: {
+    width: 1280, 
+    height: 720
+  }
+};
+
+/*
   latest captured image data
 */
 var lastImageData;
+
+/*
+  video element rendering camera input
+*/
+var cameraOutput = document.querySelector('#camera-output');
+
 /*
   capture from camera
-  returns stream object url
+  returns objectUrl
  */
 function capture() {
+  return navigator.mediaDevices.getUserMedia(constraints)
+    .then(function(stream) {
+      return window.URL.createObjectURL(stream);
+    })
+    .catch(function(err) {
+      console.log(err.name + ": " + err.message);
+    });
+}
 
+/*
+  draws stream into a video element
+  returns undefined
+*/
+function drawToVideo(stream) {
+  cameraOutput.src = stream;
 }
 
 /*
@@ -37,7 +107,7 @@ function mirror(canvas) {
   returns value
 */
 function abs(value) {
-
+  return (value ^ (value >> 31)) - (value >> 31);
 }
 
 /*
@@ -45,7 +115,7 @@ function abs(value) {
   returns 0 or 0XFF
 */
 function contrast(value, threshold) {
-
+  return (value > threshold) ? 0xFF : 0;
 }
 
 /*
@@ -60,7 +130,7 @@ function blend(canvas, width, height) {
   draw the motion image to canvas
   returns canvas
 */
-function draw(canvas, resolutionX, resolutionY) {
+function drawToCanvas(canvas, resolutionX, resolutionY) {
 
 }
 
@@ -85,5 +155,9 @@ function loop() {
   returns undefined
 */
 function initialize() {
-
+  return capture()
+    .then(drawToVideo)
+    .catch(function(error) {
+      console.error('Failed to draw camera input to video ', error);
+    });
 }
