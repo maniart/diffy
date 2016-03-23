5af04b7348a0e21874807bdc9affb924c72219ec
maniart <mani.art@gmail.com>
2016-03-22 22:56:57 -0400

    remove extra files

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            window.oRequestAnimationFrame      ||
+            window.msRequestAnimationFrame     ||
+            function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+            };
+})();
