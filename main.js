486a606e3aa8f107de932bbc411be6a82c54d64b
maniart <mani.art@gmail.com>
2016-03-23 00:04:28 -0400

    write first pass of api

--- main.js
+++ main.js
@@ -1,12 +1,89 @@
 'use strict';
 
 window.requestAnimFrame = (function(){
-    return  window.requestAnimationFrame       ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame    ||
-            window.oRequestAnimationFrame      ||
-            window.msRequestAnimationFrame     ||
-            function( callback ){
-                window.setTimeout(callback, 1000 / 60);
-            };
+  return
+    window.requestAnimationFrame       ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame    ||
+    window.oRequestAnimationFrame      ||
+    window.msRequestAnimationFrame     ||
+    function( callback ){
+      window.setTimeout(callback, 1000 / 60);
+    };
 })();
+
+/*
+  latest captured image data
+*/
+var lastImageData;
+/*
+  capture from camera
+  returns stream object url
+ */
+function capture() {
+
+}
+
+/*
+  hirozintally mirror canvas
+  returns canvas
+*/
+function mirror(canvas) {
+
+}
+
+/*
+  bitwise abs operation
+  returns value
+*/
+function abs(value) {
+
+}
+
+/*
+  adjust contrast based on value and threshold
+  returns 0 or 0XFF
+*/
+function contrast(value, threshold) {
+
+}
+
+/*
+  blend two consecutive frames
+  returns imageData
+*/
+function blend(canvas, width, height) {
+
+}
+
+/*
+  draw the motion image to canvas
+  returns canvas
+*/
+function draw(canvas, resolutionX, resolutionY) {
+
+}
+
+/*
+  center the canvas
+  returns canvas
+*/
+function center(canvas) {
+
+}
+
+/*
+  iteratively calculate and draw
+  returns undefined
+*/
+function loop() {
+
+}
+
+/*
+  kickstart the process
+  returns undefined
+*/
+function initialize() {
+
+}
